d6d1eabd43684bb23fcc6659a066edb0299099be
lxhdj <lxhdj907@163.com>
2020-08-17 00:23:59 +0800

    first

---
+++ MNIST_Model.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+class MNIST_Model(nn.Module):
+    def __init__(self):
+        super(MNIST_Model, self).__init__()  
+        self.conv1 = nn.Conv2d(1, 32, 3,)
+        self.conv2 = nn.Conv2d(32, 32, 3)
+        self.conv3 = nn.Conv2d(32, 64, 3)
+        self.conv4 = nn.Conv2d(64, 64, 3)
+        self.max_pooling1=torch.nn.MaxPool2d(2,2)
+        self.max_pooling2=torch.nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(4*4*64, 200) 
+        self.fc2 = nn.Linear(200, 10) # 10分类
+ 
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.max_pooling1(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.max_pooling2(x)
+        
+        x = x.permute((0, 2, 3, 1))
+        x = x.contiguous().view(-1, 4 * 4 * 64)
+        x = x.view(-1, 4 * 4 * 64)
+        
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, p=0.5)
+        x = self.fc2(x)
+        x = F.relu(x)
+#         x = F.log_softmax(x,dim=1)
+        return x
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CW_Attack_on_MNIST
 Implemention of cw attack on pytorch with corresponding MNIST model
+
+## MNIST model
+根据[Towards Evaluating the Robustness of Neural Networks](https://arxiv.org/abs/1608.04644) p6的TABLE1搭建  
+由四个卷积层、两个池化层、两个全连接层、激活函数ReLU组成。论文中提到的softmax不出现在model结构中，而是在loss中体现，因此此处选用CrossEntropyLoss函数
+
+## Carlini and Wagner L2
+
+论文中给出了三种攻击方式 L0, L2, L-inf,此处采用了L2攻击方式，可以修改函数
+
+```
+def loss1_func(w,x,d,c):
+    return torch.dist(x,(torch.tanh(w)*d+c),p=2)
+
+```
+以及对应的7种f(x')函数，在```def f(output,tlab,target,k=0)```中修改
+
